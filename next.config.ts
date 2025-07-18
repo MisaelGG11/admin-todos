fe6720bf8b1faea95206602a892fd7a3b253c9ac
MisaelGG11 <magomez@innovacion.gob.sv>
2025-07-18 15:56:19 -0600

    feat: add initial config for admin todos in frontend

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['tailus.io', 'api.unsplash.com'],
+  },
 };
 
 export default nextConfig;
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export * from "./sidebar/Sidebar";
+export * from "./sidebar/SidebarItem";
+export * from "./topMenu/TopMenu";
+export * from "./widgets/WidgetItem";
