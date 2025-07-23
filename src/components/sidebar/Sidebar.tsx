316d7945a64d47dd4e9abf3ff2314892ca1d07da
MisaelGG11 <magomez@innovacion.gob.sv>
2025-07-23 13:40:25 -0600

    styles: refactor styles for top menu and sidebar components

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -29,25 +29,24 @@ const menuItems = [
 
 export const Sidebar = () => {
   return (
-    <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
+    <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r border-gray-400 bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
-        <div className="-mx-6 px-6 py-4">
+        <div className="-mx-6 px-6 py-2">
           {/* TODO: Next/Link hacia dashboard */}
           <Link href="#" title="home">
             {/* Next/Image */}
             <Image
-              src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg"
-              className="w-32"
+              src="https://cdn.dealspotr.com/io-images/logo/tailus.jpg?fit=contain&trim=true&flatten=true&extend=0&width=500&height=200"
               alt="tailus logo"
-              width={150}
-              height={150}
+              width={110}
+              height={25}
             />
           </Link>
         </div>
 
         <div className="mt-8 text-center">
           <Image
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            src="https://avatars.githubusercontent.com/u/68236786?v=4"
             width={150}
             height={150}
             alt=""
@@ -66,7 +65,7 @@ export const Sidebar = () => {
         </ul>
       </div>
 
-      <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
+      <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t border-gray-400">
         <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
           <CiLogout />
           <span className="group-hover:text-gray-700">Logout</span>
--- src/components/topMenu/TopMenu.tsx
+++ src/components/topMenu/TopMenu.tsx
@@ -2,16 +2,16 @@ import { CiSearch, CiMenuBurger, CiChat1, CiBellOn } from "react-icons/ci";
 
 export const TopMenu = () => {
   return (
-    <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
+    <div className="sticky z-10 top-0 h-16 border-b border-gray-400 bg-white lg:py-2.5">
       <div className="px-6 flex items-center justify-between space-x-4">
-        <h5 hidden className="text-2xl text-gray-600 font-medium lg:block">
+        <h5 className="hidden text-2xl text-gray-600 font-medium lg:block">
           Dashboard
         </h5>
         <button className="w-12 h-16 -mr-2 border-r lg:hidden">
           <CiMenuBurger size={30} />
         </button>
         <div className="flex space-x-2">
-          <div hidden className="md:block">
+          <div className="hidden md:block">
             <div className="relative flex items-center text-gray-400 focus-within:text-cyan-400">
               <span className="absolute left-4 h-6 flex items-center pr-3 border-r border-gray-300">
                 <CiSearch />
@@ -26,13 +26,13 @@ export const TopMenu = () => {
             </div>
           </div>
 
-          <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200 md:hidden">
+          <button className="flex items-center justify-center w-10 h-10 rounded-xl border border-gray-400 bg-gray-100 focus:bg-gray-100 active:bg-gray-200 md:hidden">
             <CiSearch />
           </button>
-          <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+          <button className="flex items-center justify-center w-10 h-10 rounded-xl border border-gray-400 bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
             <CiChat1 size={25} />
           </button>
-          <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+          <button className="flex items-center justify-center w-10 h-10 rounded-xl border border-gray-400 bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
             <CiBellOn size={25} />
           </button>
         </div>
